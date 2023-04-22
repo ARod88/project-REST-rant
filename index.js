eefe4dca06db65e66ed915f88e75555d48206758
ARod88 <alma.rodriguez72@yahoo.com>
2023-04-21 21:23:02 -0700

    Adding page route

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+require('dotenv').config();
 const express = require ('require');
 const app = express()
 
 app.get('/', (req,res) => {
     res.send('Hello World!')
 })
+app.get('*', (req,res) => {
+    res.status(404).send('<h1> 404 Page</h1>')
+})
 
-app.listen(3000);
+
+app.listen(process.env.PORT);
 
