3442aefc850c6d41a0e1f847de6ffac3fe75a32d
ARod88 <alma.rodriguez72@yahoo.com>
2023-04-24 23:03:37 -0700

    Adding places view with mock data

--- controllers/places.js
+++ controllers/places.js
@@ -1,9 +1,33 @@
-const router = require ('express').Router();
+const express = require('express');
+const app = express()
 
-router.get('/', (req,res) => {
-    res.send('GET /places')
+const router = require ('express').Router()
 
-});
+
+// GET /places
+app.get('/', (req, res) => {
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+      }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+      }]
+      
+    res.render('places/index')
+  })
+  
+
+// router.get('/', (req,res) => {
+//     res.render('places/index')
+
+// });
 
 module.exports = router;
 
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require ('express');
 const app = express()
 
+
+
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine());
 
@@ -11,7 +13,7 @@ app.get('/', (req,res) => {
     res.render('home')
 })
 app.get('*', (req,res) => {
-    res.status(404).send('<h1> 404 Page</h1>')
+    res.render('error404')
 });
 
 
