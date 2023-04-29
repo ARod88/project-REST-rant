5c8ca01f77fa77ca00b3bd487e72897c9ec49bc2
ARod88 <alma.rodriguez72@yahoo.com>
2023-04-29 16:16:12 -0700

    adding styling to views

--- controllers/places.js
+++ controllers/places.js
@@ -12,18 +12,24 @@ router.get('/', (req, res) => {
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/thai-food.jpg'
       }, 
       {
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/thai-cafe.jpg'
       },
     ];
       
     res.render('places/index', {places})
+  });
+
+  //POST
+
+  router.post('/',(req,res) => {
+    res.send('POST/places')
   });
   
 
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,11 @@ const Home = () => {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <img width="400px" src="/images/parfait.jpg" alt="parfait"/>
+                <a href="/places">
+                    <button className="btn-primary">Places Page</button>
+                    </a>
+
             </main>
         </Def>
     );
