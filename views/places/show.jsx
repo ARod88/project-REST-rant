c9072e350ff6725002138a69cfaec69c5a763873
ARod88 <alma.rodriguez72@yahoo.com>
2023-04-30 02:44:01 -0700

    trying to debug edit page

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,7 +6,7 @@ const show = (data) => {
         <Def>
             <main>
                 <h1> { data.place.name }</h1>
-                <a href="`/paces/${data.id}/edit`" className="btn btn-warning">Edit</a>
+                <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>
                 <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
                     <button type="submit" className="btn btn-danger">Delete</button>
                 </form>
@@ -20,3 +20,29 @@ const show = (data) => {
 };
 
 module.exports = show;
+
+
+
+
+// const React = require('react');
+// const Def = require('../default');
+
+// const show = (data) => {
+//     return (
+//         <Def>
+//             <main>
+//                 <h1> { data.place.name }</h1>
+//                 <a href="`/paces/${data.id}/edit`" className="btn btn-warning">Edit</a>
+//                 <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+//                     <button type="submit" className="btn btn-danger">Delete</button>
+//                 </form>
+
+//                 <h2> Rating </h2>
+//                 <h2> Desription </h2>
+//             </main>
+            
+//         </Def>
+//     );
+// };
+
+// module.exports = show;
