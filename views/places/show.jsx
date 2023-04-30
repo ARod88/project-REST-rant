a1ef63b6dd03b467b6a2abe640c0b81eb16b3e28
ARod88 <alma.rodriguez72@yahoo.com>
2023-04-30 02:01:51 -0700

    delete and edit functionality bug

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,12 +1,20 @@
 const React = require('react');
 const Def = require('../default');
 
-const show = () => {
+const show = (data) => {
     return (
         <Def>
             <main>
-                <h1>Show Page</h1>
+                <h1> { data.place.name }</h1>
+                <a href="`/paces/${data.id}/edit`" className="btn btn-warning">Edit</a>
+                <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                    <button type="submit" className="btn btn-danger">Delete</button>
+                </form>
+
+                <h2> Rating </h2>
+                <h2> Desription </h2>
             </main>
+            
         </Def>
     );
 };
