fd2050b2ed58ea371abb875c60f9734c58993462
ARod88 <alma.rodriguez72@yahoo.com>
2023-04-29 19:12:54 -0700

    added forms

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -1,13 +1,96 @@
-const React = require ('react');
-const Def = require ('/default');
+const React = require('react')
+const Def = require('../default')
 
-function new_form () {
+
+
+const new_form =() => {
     return(
-        <def>
+        <Def>
+            <main>
             <h1>Add a New Place</h1>
-        </def>
+            <form method="POST" action="/places">
+  <div className="form-group">
+    <label htmlFor="name">Place Name</label>
+    <input className="form-control" id="name" name="name" required />
+  </div>
+  <div className="form-group">
+    <label htmlFor="pic">Place Picture</label>
+    <input className="form-control" id="pic" name="pic" />
+  </div>
+  <div className="form-group">
+    <label htmlFor="city">City</label>
+    <input className="form-control" id="city" name="city" />
+  </div>
+  <div className="form-group">
+    <label htmlFor="state">State</label>
+    <input className="form-control" id="state" name="state" />
+  </div>
+  <div className="form-group">
+    <label htmlFor="cuisines">Cuisines</label>
+    <input className="form-control" id="cuisines" name="cuisines" required />
+  </div>
+  <input className="btn btn-primary" type="submit" value="Add Place" />
+</form>
+
+            </main>
+            
+        </Def>
     );
 
 };
 
 module.exports = new_form;
+
+
+
+// const React = require("react");
+// const Default = require(./default');
+
+
+// const new_form =() => {
+//     return(
+//         <Def>
+//             <main>
+//             <h1>Add a New Place</h1>
+//         <form method="POST" action="/places">
+//             <div>
+//                 <label htmlFor='name'>Place Name</label>
+//                 <input id='name' name='name'/>
+//             </div>
+//             <div>
+//                 <label htmlFor='pic'>Place Picture</label>
+//                 <input type='url' id='pic' name='pic'/>  
+//             </div>
+//             <div>
+//                 <label htmlFor='city'>City</label>
+//                 <input type='city' id='city' name='city'/>  
+//             </div>
+//             <div>
+//                 <label htmlFor='state'>State</label>
+//                 <input type='state' id='state' name='state'/>  
+//             </div>
+//             <div>
+//                 <label htmlFor='cuisines'>Cuisines</label>
+//                 <input type='cuisines' id='cuisines' name='cusisines'reuired />  
+//             </div>
+//                 <input type='submit' value='Add Place' />
+
+//             </form>
+//             </main>
+            
+//         </Def>
+//     );
+
+// };
+
+// module.exports = new_form;
+
+// const Index = (data) => {
+//     let placesFormatted = data.places.map((place) => {
+//         return (
+//             <div>
+//                 <h2>{place.name}</h2>
+//                 <img src={place.pic} alt={place.name} />
+//             </div>
+//         );
+//     });
