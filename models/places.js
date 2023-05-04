4774d67e63755bce084e9f28879451263e4ccad0
ARod88 <alma.rodriguez72@yahoo.com>
2023-05-04 15:25:46 -0700

    added mongoose

--- models/places.js
+++ models/places.js
@@ -1,13 +1,32 @@
-module.exports = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/thai-food.jpg'
-}, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/thai-cafe.jpg'
-}];
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema({
+   
+    name: { type:String, required: true },
+    pic: String,
+    cuisines: { type:String, required: true },
+    city: { type:String, default: 'Anytown' },
+    state: { type:String, required: 'USA' },
+    founded: Number
+});
+
+// create bread model
+const Place = mongoose.model("Place", placeSchema);
+
+// export bread model
+module.exports = Place;
+
+
+// module.exports = [{
+//     name: 'H-Thai-ML',
+//     city: 'Seattle',
+//     state: 'WA',
+//     cuisines: 'Thai, Pan-Asian',
+//     pic: '/images/thai-food.jpg'
+// }, {
+//     name: 'Coding Cat Cafe',
+//     city: 'Phoenix',
+//     state: 'AZ',
+//     cuisines: 'Coffee, Bakery',
+//     pic: '/images/thai-cafe.jpg'
+// }];
